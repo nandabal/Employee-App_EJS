b41b70674104a0ff8dde2e84c59e81b4041bc081
Santhakumari <n.santhakumari@house-of-communication.com>
2025-09-09 15:52:47 +0530

    code clean

--- routes/basicRoutes.js
+++ routes/basicRoutes.js
@@ -4,20 +4,8 @@ router.use(express.json())
 router.use(express.urlencoded({extended:true}))
 const employeeModel=require('../model/employeeData')
 
-// const data= [
-//     { id:1,"eName":"Nanda", "eDesignation":"Software Engineer", "eSalary":50000, "eLocation":"TVM"},
-//     { id:2,"eName":"Riya", "eDesignation":"QA Lead", "eSalary":42000, "eLocation":"Kollam"},
-//     {id:3,"eName":"John", "eDesignation":"UI developer", "eSalary":30000, "eLocation":"TVM"},
-//     { id:4,"eName":"Cera", "eDesignation":"Technical Lead", "eSalary":44000, "eLocation":"EKM"},
-//     { id:5,"eName":"Manu", "eDesignation":"QA engineer", "eSalary":28000, "eLocation":"Calicut"},
-//     {id:6,"eName":"Deepak", "eDesignation":"Software Engineer", "eSalary":37000, "eLocation":"Kollam"},
-//     { id:4,"eName":"Jinu", "eDesignation":"Project Manager", "eSalary":64000, "eLocation":"EKM"},
-//     { id:5,"eName":"Anna", "eDesignation":"UI developer", "eSalary":28000, "eLocation":"TVM"},
-//     {id:6,"eName":"Sunu", "eDesignation":"Developer Intern", "eSalary":18000, "eLocation":"Kollam"}
-// ]
-
 function basicRoutes(nav) {
-  // Home - list employees
+  
   router.get("/", async (req, res) => {
     try {
       const data = await employeeModel.find();
@@ -27,12 +15,12 @@ function basicRoutes(nav) {
     }
   });
 
-  // Show Add Employee form
+ 
   router.get("/form", (req, res) => {
     res.render("EmployeeForm", { nav });
   });
 
-  // Add Employee
+ 
   router.post("/add", async (req, res) => {
     try {
       const newEmployee = new employeeModel({
@@ -48,7 +36,7 @@ function basicRoutes(nav) {
     }
   });
 
-  // Show Update Form
+  
   router.get("/update-form/:id", async (req, res) => {
     try {
       const employee = await employeeModel.findById(req.params.id);
@@ -58,7 +46,7 @@ function basicRoutes(nav) {
     }
   });
 
-  // Update Employee
+  
   router.post("/update/:id", async (req, res) => {
     try {
       await employeeModel.findByIdAndUpdate(req.params.id, {
@@ -73,7 +61,7 @@ function basicRoutes(nav) {
     }
   });
 
-  // Delete Employee
+  
   router.get("/delete/:id", async (req, res) => {
     try {
       await employeeModel.findByIdAndDelete(req.params.id);
